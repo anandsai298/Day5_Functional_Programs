91d3e9911fcda79a75b2fc81ac82a8bfa3db0dc8
anandsaikumar <anandsai298@gmail.com>
2023-03-07 10:28:49 +0530

    [sai] Add : print the percentage of head vs tail and their count

---
+++ Day5_Functional_Programs/Day5_Functional_Programs/FlipCoin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_Functional_Programs
+{
+    public class FlipCoin
+    {
+        const double VALIDATION_CHECK = 0.5;
+        public void Flip(int NoOfTimes)
+        {
+            int Head = 0, Tail = 0;
+            Random random = new Random();
+            for (int i=0;i< NoOfTimes;i++)//percentage of head and tail
+            { 
+                double Value = random.NextDouble();//default 0 to 1
+                if (Value < VALIDATION_CHECK)
+                    Tail++;
+                else
+                    Head++;
+            }
+            double Percentage_Head = (double)Head / NoOfTimes * 100;
+            double Percentage_Tail = (double)Tail / NoOfTimes * 100;
+            Console.WriteLine("Head percentage ----> " + Percentage_Head + "\n" +"count Head"+Head+ "\n" + "Tail percentage ----> " + Percentage_Tail+ "\n"+"count Tail"+Tail);
+        }
+    }
+}
--- Day5_Functional_Programs/Day5_Functional_Programs/Program.cs
+++ Day5_Functional_Programs/Day5_Functional_Programs/Program.cs
@@ -7,6 +7,26 @@ namespace Day5_Functional_Programs
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to the day_05 Functional Programs: ");
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("1.FILPCOIN \n2. EXit");
+                Console.WriteLine("enter your option to print output");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        FlipCoin fc = new FlipCoin();
+                        Console.WriteLine("enter no of flips --> ");
+                        int NoOfTimes = Convert.ToInt32(Console.ReadLine());
+                        fc.Flip(NoOfTimes);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+
+            }
         }
     }
 }
