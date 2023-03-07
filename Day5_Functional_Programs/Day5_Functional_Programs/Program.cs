7e7e6cde839daf97edd5c2c8609d05ff6fe3e6de
anandsaikumar <anandsai298@gmail.com>
2023-03-07 09:27:58 +0530

    [sai] Add : program structure is created

---
+++ Day5_Functional_Programs/Day5_Functional_Programs/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Day5_Functional_Programs
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the day_05 Functional Programs: ");
+        }
+    }
+}
